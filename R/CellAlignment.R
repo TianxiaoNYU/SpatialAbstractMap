ddcb10674b459cd96a8255c990aed07c4ed3e9bb
赵天啸 <zhaotianxiao@s-MacBook-Air.local>
2022-11-04 15:40:56 -0400

    Update package dependency

--- R/CellAlignment.R
+++ R/CellAlignment.R
@@ -23,7 +23,6 @@ CalcCor <- function(sc.data,
 #'
 #' A brief function to calculate the cosine similarity between all cells compared to one spot in ST data
 #'
-#' @import lsa
 #' @param sc.data           A data.frame indicating the scRNA-seq data; each column is a cell and each row is a gene
 #' @param ST.data.column    A vector containing the gene expression of a given spot
 #' @return A vector of cosine similarity between cells and one spot 
