fd588dec88f86d159bd70af8e2537bd14cf8a073
Tianxiao <Tianxiao.Zhao@nyulangone.org>
2022-11-10 15:32:54 -0500

    Init vignettes

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' spot_coord
+#'
+#' This is data to be included in my package
+#'
+#' @author Tianxiao Zhao \email{Tianxiao.Zhao@nyulangone.org}
+#' @keywords coordinates of spots
+'spot_coord'
+
+#' ST_expr_data
+#'
+#' The Spatial Transcriptomics data, which is a spot-by-gene data frame
+#'
+#' @author Tianxiao Zhao \email{Tianxiao.Zhao@nyulangone.org}
+#' @keywords Spatial Transcriptomics data
+'ST_expr_data'
