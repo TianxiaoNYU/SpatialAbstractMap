c78c8dd66fca287046f116284227483a37b6628b
Tianxiao <Tianxiao.Zhao@nyulangone.org>
2022-12-08 14:38:32 -0500

    Update WVD function

--- R/Voronoi.R
+++ R/Voronoi.R
@@ -6,24 +6,27 @@
 #'
 #' @importFrom broom tidy
 #' @param mds.mat             Matrix containing the position of clusters after MDS
+#' @param spatial.distribution  Cluster of tiles in a sparse form; should be a ntile-by-ncluster matrix
 #' @param centroid.weight     Numeric, weights in WVD when creating the Voronoi cells
 #' @param connection.weight   Numeric, connections whose weights greater than given values will be ploted
 #' @param grid.bound.factor   Numeric bigger than 1, how large lattice will be compared to the max value in MDS
+#' @param max.range       The max range when clustering; grids beyond this value will be clustered as "Outlier"
 #' @param ngrid               Number of grids in lattice
 #' @return A list, including the information of 1) centroids; 2) connections; 3) Voronoi cells.
 #' @export
 WVDpreprocess <- function(mds.mat,
+                          spatial.distribution,
                           centroid.weight = 0.5,
                           connection.weight = 0.3,
                           grid.bound.factor = 1.1,
+                          max.range = 10,
                           ngrid = 100){
   WVD.centroids <- as.data.frame(mds.mat) %>%
     mutate(Class = rownames(mds.mat))
   colnames(WVD.centroids)[1:2] <- c("X", "Y")
-  WVD.centroids.weight <- apply(spatial_dist, 2, function(x)(sum(x > 0)))
+  WVD.centroids.weight <- apply(spatial.distribution, 2, function(x)(sum(x > 0)))
   WVD.centroids.weight <- WVD.centroids.weight^centroid.weight
-  WVD.centroids <- WVD.centroids %>%
-    mutate(Weight = WVD.centroids.weight)
+  WVD.centroids$Weight <-  WVD.centroids.weight
 
   centroid.distance <- dist(WVD.centroids[,1:2], upper = F)
   centroid.distance <- as.data.frame(tidy(centroid.distance))
@@ -48,7 +51,7 @@ WVDpreprocess <- function(mds.mat,
                                  each = 2*grid_max/grid_unit + 1))
 
   grid_distance <- distanceGrid(WVD.centroids, WVD.grid[,1:2], weight = T)
-  grid_distance_cluster <- clusterGrid(grid_distance)
+  grid_distance_cluster <- clusterGrid(grid_distance, max.range = max.range)
   WVD.grid$Cluster <- grid_distance_cluster$Cluster
   return(list(WVD.centroids, WVD.connections, WVD.grid))
 }
@@ -72,14 +75,16 @@ weightedVoronoiPlot <- function(centroids,
     "darkturquoise", "green1", "yellow4", "yellow3", "darkorange4", "brown"
   )
   set.seed(1111)
+  if("Background" %in% grid_clusters$Cluster){
+    col_palatte <- c(c24[1:nrow(centroids)], "gray95")
+    names(col_palatte) <- c(centroids$Class, "Background")
+  }else{
+    col_palatte <- c24[1:nrow(centroids)]
+  }
   p1 <- ggplot() +
     geom_tile(data = grid_clusters,
               aes(x = X, y = Y, fill = Cluster)) +
-    scale_fill_manual(values = c24[1:nrow(centroids)])
-  # geom_text(data = centroids,
-  #            aes(x = X, y = Y, label = Class),
-  #           check_overlap = TRUE,
-  #           nudge_y = -1)
+    scale_fill_manual(values = col_palatte)
   for(i in 1:nrow(connections)){
     tmp_connections <- connections[i,]
     p1 <- p1 + geom_segment(data = tmp_connections,
@@ -117,7 +122,6 @@ weightedVoronoiPlot <- function(centroids,
 distanceGrid <- function(centroids,
                          grid_precluster,
                          weight = F){
-  # distance.matrix <- matrix(0, ncol = nrow(centroids), nrow = nrow(grid_precluster))
   distance.matrix <- apply(centroids[,1:2], 1, function(x){dista(t(x),
                                                                  grid_precluster,
                                                                  type = "euclidean")})
@@ -137,12 +141,15 @@ distanceGrid <- function(centroids,
 #' Find the nearest centroids for every tile as the cluster results.
 #'
 #' @param distance.grid   A matrix from SpatialAbstractMap::distanceGrid; include the distances of each tile to the centorids
+#' @param max.range       The max range when clustering; grids beyond this value will be clustered as "Outlier"
 #' @return A data.frame including the cluster (centroid) and the corresponding distance (weighted or not)
 #' @export
-clusterGrid <- function(distance.grid){
+clusterGrid <- function(distance.grid,
+                        max.range = 10){
   res <- apply(distance.grid, 1, function(x){
     tmp <- which.min(x)
-    return(c(x[tmp], colnames(distance.grid)[tmp]))
+    if(min(x) > max.range) return(c(x[tmp], "Background"))
+    else return(c(x[tmp], colnames(distance.grid)[tmp]))
   })
   res <- as.data.frame(t(res))
   colnames(res) <- c("Weighted_Distance", "Cluster")
