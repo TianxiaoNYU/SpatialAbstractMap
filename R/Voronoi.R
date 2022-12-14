d65089185da7c66be56f37f487bd65ec9a7123ab
Tianxiao <Tianxiao.Zhao@nyulangone.org>
2022-12-14 12:53:07 -0500

    Fix WVD bugs

--- R/Voronoi.R
+++ R/Voronoi.R
@@ -60,7 +60,7 @@ WVDpreprocess <- function(mds.mat,
 #'
 #' Draw the weighted Voronoi diagram based on the centroids, clusters and connections.
 #'
-#' @importFrom ggrepel geom_label_repel
+#' @importFrom ggrepel geom_text_repel
 #' @param centroids     A data.frame, the coordinates (X, Y) of centroids in 2-D plate; should include "Weight" column as necessary input; can include "Class" as annotations
 #' @param connections   A data.frame, indicating the coonections (correlations) between centroids; have 5 columns for start point (X, Y), end point (Xend, Yend), and the width of line (Weight)
 #' @param grid_clusters A data.frame, recording the tile coordinats (X, Y) and tile clusters (Cluster)
